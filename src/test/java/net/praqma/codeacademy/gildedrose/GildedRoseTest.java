a599cb8e666bfa2a392de319e1750f2eed15a83f
Peter <20091105@post.au.dk>
2017-08-16 15:20:29 +0200

    Lavet ny test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -13,5 +13,17 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
+	
+	@Test
+	public void fooOne(){
+		Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20)};
+
+        GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("+5 Dexterity Vest", app.items[0].name);
+		assertEquals(10, app.items[0].sellIn);
+		assertEquals(25, app.items[0].quality);
+	}
 
 }
